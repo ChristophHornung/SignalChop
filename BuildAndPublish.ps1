0ba27fed030ebd7978173592414cc4ad51f2b122
Christoph Hornung <christoph.hornung@crosberg.de>
2020-05-27 00:15:42 +0200

    👷Added zip step to build

--- BuildAndPublish.ps1
+++ BuildAndPublish.ps1
@@ -1,3 +1,23 @@
+function Compress-Subfolders
+{
+    param
+    (
+        [Parameter(Mandatory = $true)][string] $InputFolder,
+        [Parameter(Mandatory = $true)][string] $OutputFolder
+    )
+
+    $subfolders = Get-ChildItem $InputFolder | Where-Object { $_.PSIsContainer }
+
+    ForEach ($path in $subfolders) 
+    {
+        $fullpath = $path.FullName + "\*"
+        $fullpath
+        $zipname = $path.name + ".zip"
+        $zippath = $outputfolder + $zipname
+        Compress-Archive -Path $fullpath -DestinationPath $zippath
+    }
+}
+
 Remove-Item Release -Recurse -Force -EA SilentlyContinue
 dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r win-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o Release/win-x64
 dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r win-arm -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o Release/win-arm
@@ -6,3 +26,6 @@ dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r linux-x64 -c Release -
 dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r linux-arm -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o Release/linux-arm
 dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r linux-arm64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o Release/linux-arm64
 dotnet publish .\SignalChop\Crosberg.SignalChop.csproj -r osx-x64 -c Release -p:PublishSingleFile=true -p:PublishTrimmed=true -o Release/osx-x64
+Remove-Item 'Release\*' -recurse -Include *.pdb
+Compress-Subfolders Release\ Release\
+
